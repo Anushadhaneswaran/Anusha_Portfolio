31ba6d6d47f5b4cb47d85c23a51021607280b862
Anushadhaneswaran <anusha.dhanes@gmail.com>
2026-02-28 19:35:05 +0530

    Full portfolio upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openUrban(){
+    window.location.href = "urbannook.html";
+}
+
+function openVibe(){
+    window.location.href = "vibetrace.html";
+}
+
+function openNeuro(){
+    window.location.href = "neurobridge.html";
+}
+
+function goProjects(){
+    window.location.href = "projects.html";
+}
